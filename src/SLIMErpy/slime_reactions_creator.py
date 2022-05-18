13a4f7d8a78127df5badfd622efb4b796e9e0a52
jcapels <jcapels96@gmail.com>
2022-05-18 11:54:02 +0100

    [ADD] getBackboneName.m converted

--- src/SLIMErpy/slime_reactions_creator.py
+++ src/SLIMErpy/slime_reactions_creator.py
@@ -1,3 +1,5 @@
+import re
+
 import cobra
 
 
@@ -9,7 +11,7 @@ class SLIMEReactionsGenerator:
     @staticmethod
     def get_backbone_name(metabolite: cobra.Metabolite) -> str:
         """
-        Taken from getBackboneName.m
+        Taken from SLIMEr/models/getBackboneName.m
         :param metabolite:
         :return:
         """
@@ -40,7 +42,22 @@ class SLIMEReactionsGenerator:
                   'sphinganine 1-phosphate': ('long-chain base phosphate', 'endoplasmic reticulum'),
                   'phytosphingosine 1-phosphate': ('long-chain base phosphate', 'endoplasmic reticulum')
                   }
-        pass
+        metabolite_name = metabolite.name
+        # sugestão: usar expressões regulares: https://regexr.com/
+
+        x = re.search("\[[a-zA-Z0-9_ ]*\]", metabolite_name)
+        compartment_name = metabolite_name[x.start():x.end()]
+        new_metabolite_name = metabolite_name[:x.start() - 1]
+        model_backbone_name = None
+        for backbone_name in group1.keys():
+            if new_metabolite_name.startswith(backbone_name):
+                model_backbone_name = backbone_name + " " + compartment_name
+
+        for fatty_acid_name in group2:
+            if fatty_acid_name == new_metabolite_name and group2[fatty_acid_name][1] == compartment_name:
+                model_backbone_name = group2[fatty_acid_name][0] + " " + compartment_name
+
+        return model_backbone_name
 
     def add_lipid_species(self, backbone_name, formula, exchange):
         """
@@ -55,6 +72,7 @@ class SLIMEReactionsGenerator:
 
     def __main(self):
         metabolite_names = [metabolite.name for metabolite in self.model.metabolites]
+
         for metabolite in self.model.metabolites:
             backbone_name = self.get_backbone_name(metabolite)
             if backbone_name:
